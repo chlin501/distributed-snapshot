0b3025a048ada6c23c1203c82efd96b02b082e2a
My Name <me@example.com>
2017-05-13 20:54:12 +0200

    add initiator, and receiver snapshot logic.

--- singleround/src/main/scala/singleround/s.scala
+++ singleround/src/main/scala/singleround/s.scala
@@ -22,14 +22,23 @@ import akka.actor.ActorLogging
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
-
-import scala.collection.immutable.Queue
+import java.io.File
+import java.io.PrintWriter
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration.DurationInt
+import u._
 
 case object Start
 
 sealed trait Message
 case object Marker extends Message
 case class Ball(num: Int) extends Message 
+
+case object Begin
+sealed trait Snapshot
+case class ByInitiator(balls: Seq[Message]) extends Snapshot
+case class ByReceiver(balls: Seq[Message]) extends Snapshot
+//case class ChannelOnly(balls: Seq[Message]) extends Snapsnot
 
 object s {
 
@@ -45,7 +54,7 @@ object s {
     c ! a
     c ! b
 
-    // messages 
+    // balls 
 
     a ! Ball(1)
     a ! Ball(2)
@@ -58,6 +67,8 @@ object s {
     c ! Ball(7)
     c ! Ball(8)
  
+    // start snapshot procedure
+
     a ! Start
     b ! Start
     c ! Start
@@ -70,28 +81,101 @@ object Node {
   def props(id: Int): Props = Props(classOf[Node], id)
 
 }
-case class Node(id: Int) extends Actor with ActorLogging {
+class Node(id: Int) extends Actor with ActorLogging {
 
-  var neighbors = Queue.empty[ActorRef]
-  var messages = Queue.empty[Message]
+  var neighbors = Seq.empty[ActorRef]
+  var messages = Seq.empty[Message]
+  var observer: Option[ActorRef] = None 
+  val isInitiator = if (1 == id) true else false
+  var snapshotted = false
 
   override def preStart() {
-    // TODO: spwan observer 
+    observer = Option(context.system.actorOf(Observer.props(id)))
   }
 
   override def receive = initialize
 
   def initialize(): Actor.Receive = {
-    case neighbor: ActorRef => neighbors = neighbors.enqueue(neighbor)  
-    case message: Message => messages = messages.enqueue(message) 
+    case neighbor: ActorRef => neighbors ++= Seq(neighbor)  
+    case message: Message => messages ++= Seq(message) 
     case Start => {
       log.info(s"${self.path.name} has neighbors ${neighbors.map{_.path.name}}, containing messages ${messages}")
+      if(isInitiator) self ! Begin
       context.become(process) 
     }
   }
 
+  def pickedMessage(): Message = {
+    val picked = messages(pick(messages.size))
+    messages = messages diff Seq(picked)
+    picked
+  }
+
   def process: Actor.Receive = {
-    case Marker =>  
-    case Ball(num) =>
+    case Begin => {
+      observer.map { o => 
+        messages = Marker +: messages
+        o ! ByInitiator(messages) 
+        neighbors.foreach { neighbor =>
+          neighbor ! Marker
+          neighbor ! pickedMessage 
+        }
+      }
+      snapshotted = true
+      log.info(s"$id is snapshotted!")
+    }
+    case Marker => {
+      // **dummy** simulate send data before marker is received.
+      neighbors.foreach { neighbor => neighbor ! pickedMessage }   
+
+      if(!snapshotted) observer.map { o => 
+        messages = Marker +: messages 
+        o ! ByReceiver(messages)
+        neighbors.map { neighbor => neighbor ! Marker }
+        snapshotted = true
+      } else {
+        // TODO: snapshot received message before the latest received Marker  
+        observer.map { o => 
+          // TODO: find balls before marker in messages
+          //o ! ChannelOnly(ballsBeforeMarker) 
+        }  
+      }
+      neighbors.foreach(_ ! Marker)
+    }  
+    case ball: Ball => // TODO: prepend to the head in order of balls being received
+  }
+}
+
+object Observer {
+
+  def props(id: Int): Props = Props(classOf[Observer], id)
+
+}
+class Observer(id: Int) extends Actor with ActorLogging {
+
+  var count = 0
+
+  override def preStart() {
+    val dir = new File(s"/tmp/$id")
+    if(!dir.exists) {
+      log.info(s"Create directory $dir!")
+      dir.mkdirs
+    }
+  }   
+
+  def write(balls: Seq[Message])(path: String) {
+    val file = new File(path)
+    val writer = new PrintWriter(file)
+    writer.write(balls.mkString("[", ",", "]"))
+    writer.close
+  }
+ 
+  override def receive = {
+    case ByInitiator(balls) => write(balls)(s"/tmp/$id/first")
+    case ByReceiver(balls) => {
+      count += 1
+      write(balls)(s"/tmp/$id/snapshot.$count")
+    }
+    case msg@_ => log.warning(s"Observe message: $msg")
   }
 }
